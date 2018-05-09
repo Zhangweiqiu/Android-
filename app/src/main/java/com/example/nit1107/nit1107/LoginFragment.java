b6031f23ce9154fe15c2ef4bcb5e5fea7852f385
liuheng <851998288@qq.com>
2018-05-09 16:28:12 +0800

    写好了登录界面的大体框架

---
+++ app/src/main/java/com/example/nit1107/nit1107/LoginFragment.java
@@ -0,0 +1,88 @@
+package com.example.nit1107.nit1107;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LoginFragment extends Fragment {
+
+    private TextView signup;
+    private TextView forgotPassword;
+    private EditText account;
+    private EditText password;
+    private ImageButton QQ;
+    private ImageButton WeChat;
+
+
+    private Button submit;
+    @Override
+    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle saveInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_login_layout,container ,false);
+
+        //初始化各个需要使用对控件
+        signup = (TextView) view.findViewById(R.id.signup);
+        forgotPassword = (TextView) view.findViewById(R.id.forgotPassword);
+        account = (EditText) view.findViewById(R.id.account);
+        password = (EditText) view.findViewById(R.id.password);
+        submit = (Button) view.findViewById(R.id.submit);
+        QQ = (ImageButton) view.findViewById(R.id.qq_login);
+        WeChat = (ImageButton) view.findViewById(R.id.weChat_login);
+
+
+        //各个控件的点击事件
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //进入注册界面
+                ((MainActivity) getActivity()).startSignFragment();
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //进入忘记密码界面
+                ((MainActivity) getActivity()).startForgotFrament();
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isNoneEmpty())
+                {
+                    ((MainActivity)getActivity()).OnLogin(account.getText().toString(),password.getText().toString());
+                }
+                else
+                {
+                    Toast.makeText(getContext(),"账号或密码为空，请重新输入",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        return view;
+    }
+
+
+
+    private boolean isNoneEmpty()
+    {
+        return !(account.getText().toString().isEmpty() || password.getText().toString().isEmpty());
+    }
+
+
+
+}
--- app/src/main/java/com/example/nit1107/nit1107/MainActivity.java
+++ app/src/main/java/com/example/nit1107/nit1107/MainActivity.java
@@ -1,15 +1,50 @@
 package com.example.nit1107.nit1107;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginFragment loginFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginFragment = new LoginFragment();
+        startLoginFragment();
 
     }
+
+    //初使到登录界面
+    public void startLoginFragment() {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, loginFragment)
+                .commit();
+    }
+    //进入到注册界面
+    public void startSignFragment()
+    {
+
+    }
+
+    //进入忘记密码界面
+
+    public void startForgotFrament()
+    {
+
+    }
+
+    //登录事件 进入使用界面
+    public void OnLogin(String account , String password)
+    {
+
+    }
+
 }
